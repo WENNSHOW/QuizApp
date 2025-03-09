dec1d8d42d22934ce8de67188050a5518ad423cd
wennshow <vladyr55555@gmail.com>
2025-03-09 18:08:20 +0300

    Auto Admin Creation & Duplicate Email Check in quizServer app

--- quizserver/src/main/java/com/quizserver/yarosh/repository/UserRepository.java
+++ quizserver/src/main/java/com/quizserver/yarosh/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.quizserver.yarosh.repository;
 
 import com.quizserver.yarosh.entities.Users;
+import com.quizserver.yarosh.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    Users findByRole(UserRole role);
+
+    Users findFirsByEmail(String email);
 }
--- quizserver/src/main/java/com/quizserver/yarosh/service/user/UserServiceImpl.java
+++ quizserver/src/main/java/com/quizserver/yarosh/service/user/UserServiceImpl.java
@@ -1,7 +1,40 @@
 package com.quizserver.yarosh.service.user;
 
+import com.quizserver.yarosh.entities.Users;
+import com.quizserver.yarosh.enums.UserRole;
+import com.quizserver.yarosh.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    private void createAdminUser() {
+        Users optionalUser = userRepository.findByRole(UserRole.ADMIN);
+        if (optionalUser == null) {
+            Users user = new Users();
+
+            user.setName("Admin");
+            user.setEmail("admin@gmail.com");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword("admin");
+
+            userRepository.save(user);
+        }
+
+    }
+
+
+    public Boolean hasUserWithEmail(String email) {
+        return userRepository.findFirsByEmail(email) != null;
+    }
 }
