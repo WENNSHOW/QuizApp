da623667ec228235ea6abb6ea807cef4e8e5f85b
wennshow <vladyr55555@gmail.com>
2025-03-10 20:45:59 +0300

    Signup Page & Reactive Form with Validations in quizWeb app

--- quizWeb/src/app/modules/auth/signup/signup.component.ts
+++ quizWeb/src/app/modules/auth/signup/signup.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  constructor(private fb: FormBuilder,
+    private message: NzMessageService,
+    private router: Router
+  ){}
 
+  validateForm!: FormGroup;
+
+  ngOnInit(){
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      name: [null, [Validators.required]]
+    })
+  }
+
+  submitForm(){
+
+  }
 }
