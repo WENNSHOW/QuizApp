091ae0c0d678dd563ba94c24134977b3095609a0
wennshow <vladyr55555@gmail.com>
2025-03-10 21:56:55 +0300

    Create Login Reactive Form with Validations in quizWeb app

--- quizWeb/src/app/modules/auth/login/login.component.ts
+++ quizWeb/src/app/modules/auth/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router,
+  ){}
+
+  validateForm!: FormGroup;
+
+  ngOnInit(){
+    this.validateForm = this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required]
+    })
+  }
 
 }
