32408d2913094121e8aac374a95135e08fa33adf
wennshow <vladyr55555@gmail.com>
2025-03-16 14:17:19 +0300

    Completing creating and posting test in quizserver app

--- quizserver/src/main/java/com/quizserver/yarosh/controller/TestController.java
+++ quizserver/src/main/java/com/quizserver/yarosh/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.quizserver.yarosh.controller;
 
+import com.quizserver.yarosh.dto.TestDTO;
 import com.quizserver.yarosh.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/test")
+@CrossOrigin("*")
 public class TestController {
 
     private final TestService testService;
@@ -14,5 +17,14 @@ public class TestController {
     @Autowired
     public TestController(TestService testService) {
         this.testService = testService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> createTest(@RequestBody TestDTO dto) {
+        try{
+            return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
