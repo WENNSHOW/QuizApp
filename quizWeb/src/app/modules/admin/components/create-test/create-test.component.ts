544c7a1d34a0fb1f3401e2f868baed2911c202ad
wennshow <vladyr55555@gmail.com>
2025-03-16 15:05:45 +0300

    Create web admin creating test in quizweb app

--- quizWeb/src/app/modules/admin/components/create-test/create-test.component.ts
+++ quizWeb/src/app/modules/admin/components/create-test/create-test.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-create-test',
@@ -10,7 +13,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './create-test.component.scss'
 })
 export class CreateTestComponent {
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder,
+    private devicesService: AdminService,
+    private notification: NzNotificationService,
+    private router: Router,
+  ){}
 
   testForm!: FormGroup;
 
@@ -20,5 +27,26 @@ export class CreateTestComponent {
       description: [null, [Validators.required]],
       time: [null, [Validators.required]],
     })
+  }
+
+  submitForm(){
+    if(this.testForm.valid){
+      this.devicesService.createTest(this.testForm.value).subscribe(res=>{
+        this.notification
+          .success(
+            `SUCCESS`,
+            `Test Created Successfully.`,
+            { nzDuration: 5000}
+          );
+          this.router.navigateByUrl("/admin/dashboard");
+      }, error=>{
+        this.notification
+        .error(
+          `ERROR`,
+          `${error.error}`,
+          { nzDuration: 5000}
+        );
+      })
+    }
   }
 }
