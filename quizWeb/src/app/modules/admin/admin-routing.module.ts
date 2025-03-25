9f35a2780f638a9db7b1258408146e6b3cb1e11a
wennshow <vladyr55555@gmail.com>
2025-03-25 16:00:31 +0300

    Creating web interface for view all question in admin dashboard in quizWeb app

--- quizWeb/src/app/modules/admin/admin-routing.module.ts
+++ quizWeb/src/app/modules/admin/admin-routing.module.ts
@@ -5,11 +5,13 @@ import { CreateTestComponent } from './components/create-test/create-test.compon
 import { AddQuestionInTestComponent } from './components/add-question-in-test/add-question-in-test.component';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AdminGuard } from '../auth/guards/admin.guard';
+import { ViewTestComponent } from './components/view-test/view-test.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard, AdminGuard]},
   { path: 'create-test', component: CreateTestComponent, canActivate: [AuthGuard, AdminGuard]},
   { path: 'add-question/:id', component: AddQuestionInTestComponent, canActivate: [AuthGuard, AdminGuard]},
+  { path: 'view-test/:id', component: ViewTestComponent, canActivate: [AuthGuard, AdminGuard]},
 ];
 
 @NgModule({
