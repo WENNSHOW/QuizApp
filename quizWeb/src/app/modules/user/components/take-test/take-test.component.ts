ade4a2b2d137ba15e0ada70e13fc9f265670e912
wennshow <vladyr55555@gmail.com>
2025-03-25 16:50:54 +0300

    Creating user start test in quizWeb app

---
+++ quizWeb/src/app/modules/user/components/take-test/take-test.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { TestService } from '../../services/test.service';
+
+@Component({
+  selector: 'app-take-test',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './take-test.component.html',
+  styleUrl: './take-test.component.scss'
+})
+export class TakeTestComponent {
+  questions: any[] = [];
+  
+  testId: any;
+  
+  constructor(private testService: TestService,
+    private activateRoute: ActivatedRoute,
+    private message: NzMessageService,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.activateRoute.paramMap.subscribe(params =>{
+      this.testId = +params.get('id');
+
+      this.testService.getTestQuestions(this.testId).subscribe(res =>{
+        this.questions = res.questions;
+        console.log(this.questions);
+      })
+    })
+  }
+}
--- quizWeb/src/app/modules/user/user-routing.module.ts
+++ quizWeb/src/app/modules/user/user-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { TakeTestComponent } from './components/take-test/take-test.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  { path: 'take-test/:id', component: TakeTestComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
