b4a1a0672002b4bb566e55a6bd6ab6661d7f9c25
wennshow <vladyr55555@gmail.com>
2025-03-26 21:18:36 +0300

    Complete submit answers call from quizWeb app and calculate ans show time remaining in quizWeb app

--- quizWeb/src/app/modules/user/components/take-test/take-test.component.ts
+++ quizWeb/src/app/modules/user/components/take-test/take-test.component.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../../../shared/shared.module';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { TestService } from '../../services/test.service';
+import { UserStorageService } from '../../../auth/services/user-storage.service';
 
 @Component({
   selector: 'app-take-test',
@@ -17,6 +18,8 @@ export class TakeTestComponent {
   testId: any;
 
   selectedAnswers: {[key:number] : string} = {};
+
+  timeRemaining: number = 0;
   
   constructor(private testService: TestService,
     private activateRoute: ActivatedRoute,
@@ -31,12 +34,50 @@ export class TakeTestComponent {
       this.testService.getTestQuestions(this.testId).subscribe(res =>{
         this.questions = res.questions;
         console.log(this.questions);
+
+        this.timeRemaining = res.testDTO.time || 0;
       })
     })
+  }
+
+  getFormattedTime(): string{
+    const minutes = Math.floor(this.timeRemaining / 60);
+    const seconds = this.timeRemaining % 60;
+    return `${minutes}:${seconds < 10 ? '0': ''}${seconds}`;
   }
 
   onAnswerChange(questionId:number, selectedOption:string){
     this.selectedAnswers[questionId] = selectedOption;
     console.log(this.selectedAnswers);
+  }
+
+  submitAnswers(){
+    const answerList = Object.keys(this.selectedAnswers).map(questionId =>{
+      return {
+        questionId: +questionId,
+        selectedOption: this.selectedAnswers[questionId]
+      }
+    })
+
+    const data = {
+      testId: this.testId,
+      userId: UserStorageService.getUserId(),
+      responses: answerList
+    }
+
+    this.testService.submitTest(data).subscribe(res=>{
+      this.message
+      .success(
+        `Test Submitted Successfully`,
+        { nzDuration: 5000 }
+      );
+      this.router.navigateByUrl("/user/view-test-results");
+    }, error=>{
+      this.message
+      .error(
+        `${error.error}`,
+        { nzDuration: 5000 }
+      );
+    })
   }
 }
